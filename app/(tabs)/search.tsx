548773fcf4f19b780f3a5303de9c36548a7df2fd
Wage Folabessy <mbappepsg336@gmail.com>
2025-08-23 02:53:36 +0700

    search

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,128 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useIsFocused } from "@react-navigation/native";
+import { useLocalSearchParams } from "expo-router";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  Keyboard,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 const Search = () => {
+  const { focus } = useLocalSearchParams();
+  const isFocused = useIsFocused();
+  const inputRef = useRef<TextInput | null>(null);
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const {
+    data: movies,
+    loading,
+    error,
+    refetch: loadMovies,
+    reset,
+  } = useFetch(() => fetchMovies({ query: searchQuery }), false);
+
+  useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      if (searchQuery.trim()) {
+        loadMovies();
+      } else {
+        reset();
+      }
+    }, 500);
+    return () => clearTimeout(timeOutId);
+  }, [searchQuery]);
+
+  useEffect(() => {
+    if (isFocused) {
+      if (focus === "1" || focus === undefined) {
+        const t = setTimeout(() => {
+          inputRef.current?.focus();
+        }, 80);
+        return () => clearTimeout(t);
+      }
+    } else {
+      Keyboard.dismiss();
+    }
+  }, [isFocused, focus]);
+
   return (
-    <View>
-      <Text>Search</Text>
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
+      <FlatList
+        data={movies ?? []}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        className="px-5"
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+            <View className="my-5">
+              <SearchBar
+                placeholder="Search movies..."
+                value={searchQuery}
+                onChangeText={(text: string) => setSearchQuery(text)}
+                ref={inputRef}
+              />
+            </View>
+
+            {loading && (
+              <ActivityIndicator
+                size="large"
+                color="#0000ff"
+                className="my-3"
+              />
+            )}
+            {error && (
+              <Text className="text-red-500 px-5 my-3">
+                Error: {error.message}
+              </Text>
+            )}
+
+            {!loading &&
+              !error &&
+              searchQuery.trim().length > 0 &&
+              (movies ?? []).length > 0 && (
+                <Text className="text-xl text-white font-bold">
+                  Search Results for{" "}
+                  <Text className="text-accent">{searchQuery}</Text>
+                </Text>
+              )}
+          </>
+        }
+        ListEmptyComponent={
+          !loading && !error ? (
+            <View className="mt-10 px-5">
+              <Text className="text-center text-gray-500">
+                {searchQuery.trim() ? "No movies found" : "Search for a movie"}
+              </Text>
+            </View>
+          ) : null
+        }
+      />
     </View>
   );
 };
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,31 +1,51 @@
 import { icons } from "@/constants/icons";
-import React from "react";
-import { Image, TextInput, View } from "react-native";
+import React, { forwardRef } from "react";
+import {
+  Image,
+  Pressable,
+  TextInput,
+  TextInputProps,
+  View,
+} from "react-native";
 
-interface Props {
-    onPress?: () => void;
-    placeholder: string
+interface Props
+  extends Pick<TextInputProps, "placeholder" | "value" | "onChangeText"> {
+  onPress?: () => void;
+  autoFocus?: boolean;
 }
 
-const SearchBar = ({onPress, placeholder}: Props) => {
-  return (
-    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-5">
-      <Image
-        source={icons.search}
-        className="size-5"
-        resizeMode="contain"
-        tintColor="#ab8bff"
-      />
-      <TextInput
-        onPress={onPress}
-        placeholder={placeholder}
-        value=""
-        onChangeText={() => {}}
-        placeholderTextColor="#a8b5db"
-        className="flex-1 ml-2 text-white"
-      />
-    </View>
-  );
-};
+const SearchBar = forwardRef<TextInput, Props>(
+  ({ onPress, placeholder, value, onChangeText, autoFocus = false }, ref) => {
+    const isTouchable = typeof onPress === "function";
+
+    return (
+      <Pressable
+        onPress={() => {
+          onPress?.();
+        }}
+        style={{ width: "100%" }}
+      >
+        <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-5">
+          <Image
+            source={icons.search}
+            className="size-5"
+            resizeMode="contain"
+            tintColor="#ab8bff"
+          />
+          <TextInput
+            ref={ref}
+            autoFocus={autoFocus}
+            placeholder={placeholder}
+            value={value}
+            onChangeText={onChangeText}
+            editable={!isTouchable}
+            placeholderTextColor="#a8b5db"
+            className="flex-1 ml-2 text-white"
+          />
+        </View>
+      </Pressable>
+    );
+  }
+);
 
 export default SearchBar;
